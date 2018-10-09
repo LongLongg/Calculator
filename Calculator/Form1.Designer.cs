67e4f609c8acfab1eb1c469fce43f2f268131199
linhdang9896 <linhdangjs.96@gmail.com>
2018-10-09 14:14:14 +0700

    voi vang qua vay

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -33,60 +33,88 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Agency FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(280, 52);
+            this.button1.Location = new System.Drawing.Point(560, 49);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 36);
+            this.button1.Size = new System.Drawing.Size(154, 34);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Tính";
+            this.button1.Text = "Trừ";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(124, 48);
+            this.textBox1.Location = new System.Drawing.Point(248, 45);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(111, 23);
+            this.textBox1.Size = new System.Drawing.Size(218, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(124, 77);
+            this.textBox2.Location = new System.Drawing.Point(248, 72);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(111, 23);
+            this.textBox2.Size = new System.Drawing.Size(218, 22);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 52);
+            this.label1.Location = new System.Drawing.Point(26, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 18);
+            this.label1.Size = new System.Drawing.Size(61, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "So thu 1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 82);
+            this.label2.Location = new System.Drawing.Point(26, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.Size = new System.Drawing.Size(61, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "So thu 2";
             // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(560, 106);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(153, 34);
+            this.btnCong.TabIndex = 3;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(68, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 22);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Kết quả: ";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(4F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 174);
+            this.ClientSize = new System.Drawing.Size(826, 164);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnCong);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6, 3, 6, 3);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -101,6 +129,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
